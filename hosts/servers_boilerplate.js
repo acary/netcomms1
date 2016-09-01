3539e2efa88df6df3f2f8103da77a3d10f3d3980
acary <dwc3452@gmail.com>
2016-09-01 13:11:40 -0700

    correct syntax

--- hosts/servers_boilerplate.js
+++ hosts/servers_boilerplate.js
@@ -1,5 +1,5 @@
-var server1 = {name: primary_one, version: 1.0.0};
-var server2 = {name: primary_two, version: 1.0.0};
+var server1 = {name: "primary_one", IP: "192.168.0.0"};
+var server2 = {name: "primary_two", IP: "192.168.0.1"};
 
-return server1;
-return server2;
+alert(server1);
+alert(server2);
